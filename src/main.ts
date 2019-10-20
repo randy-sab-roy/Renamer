f8a5d1c3309140ac0d07f1b9fea0a9422419ff06
Randy Sab-Roy <randy.sab.roy@gmail.com>
2019-10-20 11:57:01 -0400

    Initiate project

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { app, BrowserWindow, Menu } from "electron";
+import * as path from "path";
+
+let mainWindow: BrowserWindow;
+
+function init(): void {
+    mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 700,
+        icon: path.join(__dirname, "../assets/icon.png"),
+        webPreferences: { nodeIntegration: true },
+    });
+    mainWindow.setMenu(null);
+    mainWindow.loadFile(path.join(__dirname, "../html/index.html"));
+    mainWindow.on("close", () => { mainWindow = null });
+
+    if (!app.isPackaged) {
+        mainWindow.setMenu(Menu.buildFromTemplate([{
+            label: "Options",
+            submenu: [
+                { label: "Dev Console", accelerator: "F12", click: () => mainWindow.webContents.openDevTools() },
+                { label: "Reload", accelerator: "CommandOrControl+R", click: () => mainWindow.reload() }
+            ]
+        }]));
+    }
+
+    mainWindow.show();
+}
+
+app.on("ready", init);
