e15cfa3043e619b3a86b95e9b2af3238cb5092ba
Randy Sab-Roy <randy.sab.roy@gmail.com>
2019-10-20 16:54:40 -0400

    Allow to browse

--- src/renamer.ts
+++ src/renamer.ts
@@ -1,9 +1,32 @@
 import { remote } from "electron"
+import * as fs from "fs";
+
+let filesToRename: Map<string, string> = new Map();
+
+function updateFilesDisplay(): void {
+    let fileList = $("#file-list");
+    let replaceList = $("#replace-list");
+
+    fileList.html("");
+    replaceList.html("");
+
+    for (let [key, value] of filesToRename) {
+        fileList.append($("<li></li>").attr("class", "list-group-item").text(key))
+        replaceList.append($("<li></li>").attr("class", "list-group-item").text(value))
+    }
+}
 
 $("#browse").on("click", () => {
-    remote.dialog.showOpenDialog({ properties: ['openDirectory'] }).then(v => {
-        if (v != null) {
-            $("#path").text(v.filePaths[0]);
+    remote.dialog.showOpenDialog({ properties: ['openDirectory'] }).then(dialogValue => {
+        if (dialogValue != null) {
+            let path = dialogValue.filePaths[0];
+            $("#path").text(path);
+
+            filesToRename.clear();
+            fs.readdir(path, (_, files) => {
+                files.forEach(f => filesToRename.set(f, f));
+                updateFilesDisplay();
+            })
         }
     });
 })
